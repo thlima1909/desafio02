b39ccae934611e3268b0dd5a323ac41d2614eb55
Thiago Lima <thlima1909@gmail.com>
2025-10-16 08:27:35 -0300

    resolvendo conflitos

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ console.log(numeroMaior);
 function exibirMult(n) {
     return n * n;
 }
-let numeroMultiplicado = exibirMult(5);
+let numeroMultiplicado = exibirMult(2);
 console.log(numeroMultiplicado);
 
 
